263e4cfa960877f446b3a45ed3356805cb377b10
Nate McGregor <natecmcgregor@gmail.com>
2017-09-08 12:33:05 -0700

    test and solve antigrams method

--- lib/Anagrams_antigrams.rb
+++ lib/Anagrams_antigrams.rb
@@ -1,6 +1,11 @@
 #! usr/bin/env ruby
 
 class Anagrams_antigrams
+
+  # def highestCount(hash)
+  #   hash.max_by{|k,v| v}
+  # end
+
   def anagram(word1,word2)
     origin1 = word1
     origin2 = word2
@@ -17,10 +22,33 @@ class Anagrams_antigrams
   end
 
   def antigram(word1,word2)
+    word1 = word1.downcase.gsub(/[^a-z0-9]/i, '')
+    word2 = word2.downcase.gsub(/[^a-z0-9]/i, '')
+    word1 = word1.split("").sort
+    word2 = word2.split("").sort
+    both = (word1 + word2).sort
 
+    count1 = Hash.new 0
+    word1.each do |letter|
+      count1[letter] += 1
+    end
+    count2 = Hash.new 0
+    word2.each do |letter|
+      count2[letter] += 1
+    end
+    addAll = count1.merge(count2)
+    if count1.length + count2.length == addAll.length
+      return "These words are antigrams"
+    else
+      return "These words are not antigrams"
+    end
   end
 
   def palindrome(word1,word2)
+
+  end
+
+  def isWord?(word1,word2)
 
   end
 end
--- spec/Anagrams_antigrams_spec.rb
+++ spec/Anagrams_antigrams_spec.rb
@@ -8,16 +8,19 @@ describe('Anagrams_antigrams') do
     expect(check.anagram("aligned","dealing")).to(eq("These words are anagrams"))
   end
 
-  it("Will check to see if two words provided contain exact same letters, but in differrent orders") do
+  it("Will check to see if two words provided do not contain exact same letters, but in differrent orders") do
     expect(check.anagram("dog","dog")).to(eq("These words are not anagrams"))
   end
 
-  # it("Will check to see if two words provided contain exact same letters in reverse order") do
-  #   expect(palindrome("racecar","racecar")).to(eq("These words are palindromes"))
-  # end
-  #
-  # it("Will check to see if two words provided contain none of the same letters") do
-  #   expect(antigram("fox","bun")).to(eq("These words are antigrams"))
-  # end
+  it("Will check to see if two words provided contain none of the same letters") do
+    expect(check.antigram("motor","slick")).to(eq("These words are antigrams"))
+  end
+  it("Will check to see if two words provided contain some of the same letters") do
+    expect(check.antigram("boxer","fox")).to(eq("These words are not antigrams"))
+  end
 
+    # it("Will check to see if two words provided contain exact same letters in reverse order") do
+    #   expect(palindrome("racecar","racecar")).to(eq("These words are palindromes"))
+    # end
+    #
 end
